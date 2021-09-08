c928f979706a93f932752c6af364b0b338b14ace
chinoll <chinoll@chinoll.org>
2021-09-08 18:16:43 +0800

    添加默认代理IP池

--- index.js
+++ index.js
@@ -7,24 +7,35 @@ const async = require("async")
 const util = require("util")
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 const delay_time = 5000
-
+const proxy_url = "http://demo.spiderpy.cn/get/"
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+if(proxy_url == "http://demo.spiderpy.cn/get/") {
+    console.log("警告:该获取代理IP池为proxy_pool项目(https://github.com/jhao104/proxy_pool)的测试地址，请更换为本地的代理IP池")
+    readline.question("按下任意键继续",(answer) => {})
+}
 async function download_image(tags,page_count) {
     let base_url = util.format("https://safebooru.donmai.us/posts?page=%d&tags=%s",page_count,tags)
     await axios({
         method:"get",
-        url:base_url
+        url:base_url,
+        proxy:await get_proxy()
     }).then(async (respone) => {
         $ = cheerio.load(respone.data)
         let html = $("#posts-container").html()
         let reg = /https:\/\/cdn.donmai.us\/original\/.{2}\/.{2}\/[a-zA-Z0-9]*\.jpg/g
         let link_arr = html.match(reg)
+        let proxy = await get_proxy()
         async.mapLimit(link_arr,link_arr.length,async (url) => {
             let image_name = url.split("/")
             image_name = image_name[image_name.length - 1]
             await axios({
                 method:'get',
                 url:url,
-                responseType:'arraybuffer'
+                responseType:'arraybuffer',
+                proxy:proxy
             }).then((response) => {
                 console.log("download",url)
                 fs.writeFile(image_name,response.data,'binary',() => {})
@@ -55,7 +66,10 @@ async function download2(keyword) {
         await download_image(keyword,page_count)
     }
 }
-
+async function get_proxy() {
+    let data = (await axios.get("http://demo.spiderpy.cn/get/")).data['proxy'].split(":")
+    return {host:data[0],port:data[1]}
+}
 async function download_tag_list(keyword_list,concurrent) {
     for(let i = 0;i < keyword_list.length;i++) {
         await download(keyword_list[i],concurrent)
